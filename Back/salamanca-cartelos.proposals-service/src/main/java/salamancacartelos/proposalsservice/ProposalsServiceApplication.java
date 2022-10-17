361a92707adc7b97fd1cf6f402c68ac823b64fcb
Tracks12 <cardinal.florian.erwan@gmail.com>
2022-10-17 15:15:37 +0200

    add proposals API source code pre shoot

---
+++ Back/salamanca-cartelos.proposals-service/src/main/java/salamancacartelos/proposalsservice/ProposalsServiceApplication.java
@@ -0,0 +1,13 @@
+package salamancacartelos.proposalsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposalsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProposalsServiceApplication.class, args);
+	}
+
+}
---
+++ Back/salamanca-cartelos.proposals-service/src/test/java/salamancacartelos/proposalsservice/ProposalsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package salamancacartelos.proposalsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
